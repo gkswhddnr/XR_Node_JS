a52a6721b5cb2dcd574aae79e9f60f241ae9688c
gkswhddnr <gkswhddnr7593@naver.com>
2023-11-22 12:54:57 +0900

    feat: node 삭제된 파일들 다시 업로드

--- Node/Main.js
+++ Node/Main.js
@@ -1,7 +1,7 @@
-var http = require("http"); //HTTP 모듈을 가져온다
+var http = require("http"); //HTTP 모듈을 가져온다. 
 
-// 서버 동작시키고 포트 8000번을 연다.
-http.createServer(function(request, response)
+//서버 동작시키고 포트 8000번을 연다. 
+http.createServer(function (request, response)
 {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
